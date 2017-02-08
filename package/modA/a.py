69368e3e69b8f832b14b0c8db9ad6c939c22c533
Vladimir Korshunov <bekobou@yandex.ru>
2017-02-09 02:51:55 +0300

    Fix import nested directory for Python 3

--- package/modA/a.py
+++ package/modA/a.py
@@ -1,7 +1,7 @@
 from ..modB.b import SECRET
 from ..c import ANOTHER_SECRET
 from .d import QUESTION
-from modAX.x import say_answer
+from .modAX.x import say_answer
 
 def say_secret():
     print(SECRET + " " + ANOTHER_SECRET)
